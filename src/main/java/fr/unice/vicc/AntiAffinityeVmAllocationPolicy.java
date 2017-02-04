0c56b9bbb8566295b79071be4b8020d5b435b793
Arshak Mehrabyan <sdi1100049@di.uoa.gr>
2017-02-04 14:48:18 +0100

    base

--- src/main/java/fr/unice/vicc/AntiAffinityeVmAllocationPolicy.java
+++ src/main/java/fr/unice/vicc/AntiAffinityeVmAllocationPolicy.java
@@ -5,6 +5,7 @@ import org.cloudbus.cloudsim.Vm;
 import org.cloudbus.cloudsim.VmAllocationPolicy;
 import org.cloudbus.cloudsim.power.PowerHost;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,37 +13,65 @@ import java.util.Map;
  * Created by arsha on 30-Jan-17.
  */
 public class AntiAffinityeVmAllocationPolicy extends VmAllocationPolicy {
+
+    /** The map to track the server that host each running VM. */
+    private Map<Vm, Host> hoster;
+
     public AntiAffinityeVmAllocationPolicy(List<PowerHost> hosts) {
+
         super(hosts);
+        hoster =new HashMap<>();
     }
 
     @Override
     public boolean allocateHostForVm(Vm vm) {
+
+        for (Host host : getHostList()){
+            if(host.vmCreate(vm)) {
+                hoster.put(vm, host);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean allocateHostForVm(Vm vm, Host host) {
+
+        if(host.vmCreate(vm)) {
+            hoster.put(vm, host);
+            return true;
+        }
         return false;
     }
 
     @Override
     public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> list) {
+
         return null;
     }
 
     @Override
     public void deallocateHostForVm(Vm vm) {
 
+        hoster.get(vm).vmDestroy(vm);
+        hoster.remove(vm);
     }
 
     @Override
     public Host getHost(Vm vm) {
-        return null;
+
+        return vm.getHost();
     }
 
     @Override
-    public Host getHost(int i, int i1) {
+    public Host getHost(int vmId, int userId) {
+
+        for (Host h: getHostList()){
+            if (h.getVm(vmId,userId) != null){
+                return h;
+            }
+        }
         return null;
     }
 }
