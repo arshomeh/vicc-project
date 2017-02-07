0d08e4bdb5348989e1696d915014b9c606c6d36f
ManfrediG <manfredi.giordano@me.com>
2017-02-07 11:40:37 +0100

    Log messages added

--- src/main/java/fr/unice/vicc/scheduler/AntiAffinityVmAllocationPolicy.java
+++ src/main/java/fr/unice/vicc/scheduler/AntiAffinityVmAllocationPolicy.java
@@ -13,72 +13,88 @@ import java.util.Map;
 /**
  * Created by arsha on 30-Jan-17.
  * 
- * Scheduler purpose: ...
- * Design choice: ... 
+ * Scheduler purpose: no two hosts from the same affinity interval have to be allocated to the same VM
+ * Design choice: the assignment to the VM to an host is made with the modulo operation
  * Worst-case temporal complexity: ...
  */
 public class AntiAffinityVmAllocationPolicy extends VmAllocationPolicy {
 
+	/** Interval for affinity */
+    final static private int interval = 100;
+    
     /** The map to track the server that host each running VM. */
     private Map<Vm, Host> hoster;
-    private final static int interval = 100;
 
     public AntiAffinityVmAllocationPolicy(List<? extends Host> hosts) {
-
         super(hosts);
         hoster = new HashMap<>();
     }
 
     @Override
     public boolean allocateHostForVm(Vm vm) {
-    	// no two hosts from the same interval have to be allocated to the same VM
-        int value = vm.getId()% interval;
-        Host host = getHostList().get(value);
-        System.out.println("VM " + vm.getId() + " in host " + host.getId());
-        if(host != null && host.vmCreate(vm)) {
-                hoster.put(vm, host);
-                return true;
-            }
+        int wantedHostId = vm.getId() % interval;
+        Host host = getHostList().get(wantedHostId);
+        Log.print(
+        		String.format("Trying to allocate VM %d in host %d",
+        				vm.getId(),
+        				host.getId()));
+        
+        // check if the host can create the VM
+        if(host.vmCreate(vm)) {
+            hoster.put(vm, host);
+            System.out.println(String.format("[+] Allocated VM %d in host %d", vm.getId(), host.getId()));
+            return true;
+        }
+        
+        // no appropriate host found
+        System.err.println(
+        		String.format("[-] Cannot allocate VM %d on wanted host %d",
+        				vm.getId(),
+        				wantedHostId));
         return false;
     }
 
     @Override
     public boolean allocateHostForVm(Vm vm, Host host) {
+        int wantedHostId = vm.getId() % interval;
+        Log.print(
+        		String.format("Trying to allocate VM %d in host %d (requested host: %d)",
+        				vm.getId(),
+        				wantedHostId,
+        				host.getId()));
 
-        int value = vm.getId()% interval;
-        System.out.println("VM " + vm.getId() + " in host " + host.getId());
-
-        if(host.getId() == value && host.vmCreate(vm)) {
+        // check if the host can create the VM
+        if(host.getId() == wantedHostId && host.vmCreate(vm)) {
             hoster.put(vm, host);
+            System.out.println(String.format("[+] Allocated VM %d in host %d", vm.getId(), host.getId()));
             return true;
         }
+        
+        // no appropriate host found
+        System.err.println(String.format("[-] Cannot allocate VM %d on host %d", vm.getId(), host.getId()));
         return false;
     }
 
     @Override
     public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> list) {
-
         return null;
     }
 
     @Override
     public void deallocateHostForVm(Vm vm) {
-
-    	Host toRemove = vm.getHost();
-    	toRemove.vmDestroy(vm);
-    	hoster.remove(vm, toRemove);
+    	Host hostToRemove = vm.getHost();
+    	hostToRemove.vmDestroy(vm);
+    	hoster.remove(vm, hostToRemove);
     }
 
     @Override
     public Host getHost(Vm vm) {
-
         return vm.getHost();
     }
 
     @Override
     public Host getHost(int vmId, int userId) {
-
-        for (Host h: getHostList()){
+        for(Host h: getHostList()) {
             if (h.getVm(vmId,userId) != null){
                 return h;
             }
