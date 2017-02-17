c345429b4478054125960d09c91a3546a57f7cec
Arshak Mehrabyan <sdi1100049@di.uoa.gr>
2017-02-17 12:35:54 +0100

    1st version

--- src/main/java/fr/unice/vicc/scheduler/DisasterRecoveryVmAllocationPolicty.java
+++ src/main/java/fr/unice/vicc/scheduler/DisasterRecoveryVmAllocationPolicty.java
@@ -16,48 +16,87 @@ import org.cloudbus.cloudsim.VmAllocationPolicy;
  */
 public class DisasterRecoveryVmAllocationPolicty extends VmAllocationPolicy {
 
-    /** The map to track the server that host each running VM. */
-    private Map<Vm, Host> hoster;
+	/**
+	 * The map to track the server that host each running VM.
+	 */
+	private Map<Vm, Host> hoster;
+	int prev;
 
-	public DisasterRecoveryVmAllocationPolicty(List<? extends Host> hosts) {
-		super(hosts);
-        hoster = new HashMap<>();
+	public DisasterRecoveryVmAllocationPolicty(List<? extends Host> list) {
+
+		super(list);
+		hoster = new HashMap<>();
+		prev = -1;
 	}
 
 	@Override
-	public boolean allocateHostForVm(Vm arg0) {
-		// TODO Auto-generated method stub
+	protected void setHostList(List<? extends Host> hostList) {
+
+		super.setHostList(hostList);
+		hoster = new HashMap<>();
+	}
+
+	@Override
+	public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> list) {
+		// no optimizations in the naive allocation
+		return null;
+	}
+
+	@Override
+	public boolean allocateHostForVm(Vm vm) {
+		// the first host in the host list having enough resources will be the one allocated
+		for (Host host : getHostList()) {
+			int hostFamily = host.getTotalMips();
+			if(hostFamily != prev) {
+				if (host.vmCreate(vm)) {
+					hoster.put(vm, host);
+					prev = hostFamily;
+//                System.out.println("VM " + host.getId() + " allocated");
+					return true;
+				}
+			}
+		}
+
+		// no appropriate host found!
 		return false;
 	}
 
 	@Override
-	public boolean allocateHostForVm(Vm arg0, Host arg1) {
-		// TODO Auto-generated method stub
+	public boolean allocateHostForVm(Vm vm, Host host) {
+
+		int hostFamily = host.getTotalMips();
+		if (hostFamily == prev)
+			return false;
+		if (host.vmCreate(vm)) {
+			hoster.put(vm, host);
+			prev = hostFamily;
+			return true;
+		}
 		return false;
 	}
 
 	@Override
-	public void deallocateHostForVm(Vm arg0) {
-		// TODO Auto-generated method stub
+	public void deallocateHostForVm(Vm vm) {
 
+		Host toRemove = vm.getHost();
+		toRemove.vmDestroy(vm);
+		hoster.remove(vm, toRemove);
 	}
 
 	@Override
-	public Host getHost(Vm arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Host getHost(Vm vm) {
+
+		return vm.getHost();
 	}
 
 	@Override
-	public Host getHost(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+	public Host getHost(int vmId, int userId) {
+
+		for (Host h : getHostList()) {
+			if (h.getVm(vmId, userId) != null) {
+				return h;
+			}
+		}
 		return null;
 	}
-
-	@Override
-	public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
