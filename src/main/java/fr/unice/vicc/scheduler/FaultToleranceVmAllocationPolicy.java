1c85c4081a8f329e0e229879ecd1a15d5c7d37f0
Leila <leyla.myosotis@gmail.com>
2017-02-17 18:44:09 +0100

    Fault Tolerance implementation

--- src/main/java/fr/unice/vicc/scheduler/FaultToleranceVmAllocationPolicy.java
+++ src/main/java/fr/unice/vicc/scheduler/FaultToleranceVmAllocationPolicy.java
@@ -1,5 +1,6 @@
 package fr.unice.vicc.scheduler;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,45 +9,83 @@ import org.cloudbus.cloudsim.Vm;
 import org.cloudbus.cloudsim.VmAllocationPolicy;
 
 /**
- * Created by Leila K. on 01/02/2017.
- * 
- * Scheduler purpose: ...
- * Design choice: ... 
- * Worst-case temporal complexity: ...
+ * Created by Leila K. on 01/02/2017
  */
 public class FaultToleranceVmAllocationPolicy extends VmAllocationPolicy {
+	private Map<Vm, Host> hoster;
+    private final static int multiple = 10;
 
 	public FaultToleranceVmAllocationPolicy(List<? extends Host> hosts) {
 		super(hosts);
+		hoster = new HashMap<>();
 	}
 
-	@Override
-	public boolean allocateHostForVm(Vm arg0) {
-		return false;
+	private boolean createHost(Host host, Vm vm) {
+		if (host.vmCreate(vm)) {
+            hoster.put(vm, host);
+            //System.out.println("VM " + vm.getId() + " allocated on " + host.getId());
+            return true;
+        }
+		return false; 
 	}
-
+	
 	@Override
-	public boolean allocateHostForVm(Vm arg0, Host arg1) {
-		return false;
-	}
-
-	@Override
-	public void deallocateHostForVm(Vm arg0) {
+	public boolean allocateHostForVm(Vm vm) {
+		int vmId = vm.getId();
+		boolean bCheckIfSuitable = false; 
 		
+		if (vmId % multiple == 0)
+			bCheckIfSuitable = true; 
+        
+        for (Host host : getHostList()) {
+        	if (bCheckIfSuitable) {
+        		if (!host.isSuitableForVm(vm))
+        			continue; //allocate only if host is suitable 
+        		//otherwise we allocate on any host (actually on the first where creation was successful) 
+        	} 
+ 
+        	if (createHost(host, vm)) {
+                return true;
+            }   		
+        } 
+        
+        return false; 
+	}
+
+     
+	@Override
+	public boolean allocateHostForVm(Vm vm, Host host) {
+		//if we should ensure the fault tolerance for this VM but host is not appropriate
+		if ((vm.getId() % multiple == 0) && !host.isSuitableForVm(vm))
+			return false;
+		
+		return createHost(host, vm);  
 	}
 
 	@Override
-	public Host getHost(Vm arg0) {
-		return null;
+	public void deallocateHostForVm(Vm vm) {
+    	Host toRemove = vm.getHost();
+    	toRemove.vmDestroy(vm);
+    	hoster.remove(vm, toRemove);
 	}
 
 	@Override
-	public Host getHost(int arg0, int arg1) {
-		return null;
+	public Host getHost(Vm vm) {
+		return vm.getHost();
 	}
 
 	@Override
-	public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> arg0) {
+	public Host getHost(int vmId, int userId) {
+        for (Host h: getHostList()) {
+            if (h.getVm(vmId, userId) != null) {
+                return h;
+            }
+        }
+        return null;
+	}
+
+	@Override
+	public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> list) {
 		return null;
 	}
 
