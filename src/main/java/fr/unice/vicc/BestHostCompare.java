42c1995956d329de50b307d973a5067ae659a22c
Arshak Mehrabyan <sdi1100049@di.uoa.gr>
2017-02-17 20:42:53 +0100

    optimized

--- src/main/java/fr/unice/vicc/BestHostCompare.java
+++ src/main/java/fr/unice/vicc/BestHostCompare.java
@@ -11,41 +11,34 @@ import java.util.Comparator;
  * Comparator for Best-Fit scheduling algorithm.
  */
 public class BestHostCompare implements Comparator<Host> {
-	
+
     @Override
     public int compare(Host h1, Host h2) {
 
         PowerHost host1 = (PowerHost) h1;
         PowerHost host2 = (PowerHost) h2;
 
-        if (host1.getMaxPower() < host2.getMaxPower())
-            return -1;
-        else if (host1.getMaxPower() > host2.getMaxPower())
-            return 1;
-        else {
-            double cpu1 = h1.getAvailableMips();
-            double cpu2 = h2.getAvailableMips();
+        if ((host1.getMaxPower() - host2.getMaxPower()) != 0)
+            return (int) (host1.getMaxPower() - host2.getMaxPower());
 
-            double ram1 = h1.getRamProvisioner().getAvailableRam();
-            double ram2 = h2.getRamProvisioner().getAvailableRam();
+        double cpu1 = h1.getAvailableMips();
+        double cpu2 = h2.getAvailableMips();
 
-            // weights of the two dimensions
-            double x = 0.7;
-            double y = 0.3;
+        double ram1 = h1.getRamProvisioner().getAvailableRam();
+        double ram2 = h2.getRamProvisioner().getAvailableRam();
 
-            // linear combination
-            double total1 = cpu1 * y + ram1 * x;
-            double total2 = cpu2 * y + ram2 * x;
+        // weights of the two dimensions
+        double x = 0.7;
+        double y = 0.3;
 
-//        System.out.println("host 1: memory " + ram1 + " CPU " + cpu1 + " overall score " + total1);
-//        System.out.println("host 2: memory " + ram2 + " CPU " + cpu2 + " overall score " + total2);
+        // linear combination
+        double total1 = cpu1 * y + ram1 * x;
+        double total2 = cpu2 * y + ram2 * x;
+
+//        System.out.println("host 1  " + ram1 +" "+ cpu1 + " " + total1);
+//        System.out.println("host 2  " + ram2 +" "+ cpu2 + " " + total2);
 //        System.out.println("--------------------------------------------");
-            if (total1 < total2)
-                return -1;
-            else if (total1 > total2)
-                return 1;
-            return 0;
-        }
+        return (int) (total1 - total2);
+
     }
-    
 }
